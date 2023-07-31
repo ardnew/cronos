c457319a4b3c64106b4503d621c216d1b6c2fc48
ardnew <andrew@ardnew.com>
2023-07-30 19:03:30 -0500

    broken: invalid default template parameters

--- ticks.h
+++ ticks.h
@@ -4,40 +4,50 @@
 #include <chrono>
 #include <cstdint>
 
-class native {
-  friend struct ticks;
-
-protected:
+struct native {
 #if defined(ARDUINO)
 #include <Arduino.h>
-  static const auto get = millis;
+#define _get (millis) // Arduino ticks are in milliseconds.
   using per = std::milli;
 #elif defined(ESP_PLATFORM)
-#include <esp_timer.h>
-  static const auto get = esp_timer_get_time;
+#include <esp_timer.h> // ESP32 (FreeRTOS) ticks are in microseconds.
+#define _get (esp_timer_get_time)
   using per = std::micro;
 #endif
-  // A std::chrono::duration wrapping the platform's native timer API.
-  using rep = decltype(get());
-  using duration = std::chrono::duration<rep, per>;
+  // Type dur is a std::chrono::duration represented using the platform's native
+  // timer API. The precision and period of all templated ticks classes are
+  // limited to dur internally, but they can be accessed and used according to
+  // the parameters of ticks they were declared with.
+  using rep = decltype(_get());
+  using dur = std::chrono::duration<rep, per>;
+
   template <typename ToRep, typename ToPer>
   static inline constexpr const std::chrono::duration<ToRep, ToPer> &
-  at(const duration &d) {
+  at(const dur &d) {
     return std::chrono::duration_cast<std::chrono::duration<ToRep, ToPer>>(d);
   }
   template <typename ToRep, typename ToPer>
   static inline const std::chrono::duration<ToRep, ToPer> &now() {
-    return at<ToRep, ToPer>(duration(get()));
+    return at<ToRep, ToPer>(dur(_get()));
   }
+
+  // Give all generated ticks classes access to all native members.
+  template <typename ToRep, typename ToPer> friend struct ticks;
 };
 
-// The current platform's native ticks — in periods of 1× native::per
-// represented with type native::rep — are translated at compile-time into the
-// desired ticks — in periods of 1× ToPer, represented with type ToRep —
-// according to the following template parameters.
+// The current platform's native ticks are always in periods of 1× native::per
+// represented with type native::rep.
+// If the user chooses to not specify template parameters, the default period
+// and storage representation from native are used.
+// Otherwise, the user can specify a period and representation to use, and the
+// appropriate conversion will be performed automatically.
 template <typename ToRep = native::rep, typename ToPer = native::per>
 struct ticks : std::chrono::duration<ToRep, ToPer> {
-  inline ticks() : std::chrono::duration(native::now<ToRep, ToPer>()) {}
+  using std::chrono::duration<ToRep, ToPer>::duration;
+  using rep = ToRep;
+  using per = ToPer;
+  inline ticks():
+    std::chrono::duration<ToRep, ToPer>(native::now<ToRep, ToPer>()) {}
 };
 
 #endif // ticks_h
